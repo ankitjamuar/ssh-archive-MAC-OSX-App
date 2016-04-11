481ba5235132b9a1f9b581677c874fd17b1cda28
Ankit Sinha <info@tagbin.in>
2016-04-11 15:43:39 +0530

    Initial Commit

---
+++ SSH Archive/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SSH Archive
+//
+//  Created by Ankit Sinha on 11/04/16.
+//  Copyright © 2016 Ankit Sinha. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SSH Archive/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SSH Archive
+//
+//  Created by Ankit Sinha on 11/04/16.
+//  Copyright © 2016 Ankit Sinha. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
