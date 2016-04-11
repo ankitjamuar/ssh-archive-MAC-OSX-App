46ac564f413b4d5ca2de37304de65f861581ec70
Ankit Sinha <info@tagbin.in>
2016-04-11 17:23:29 +0530

    UI complete

--- SSH Archive/AppDelegate.swift
+++ SSH Archive/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
+        let task = NSTask()
+        task.launchPath = "/bin/sh"
+        task.arguments = ["-c", "ssh bitnami@tagbin.in -i /Users/ankitsinha/Work/PPK/TAGBIN/tagbin.pem"]
+        //task.launch()
+        //task.waitUntilExit()
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
